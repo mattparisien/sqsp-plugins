f34106ed411a53323beb6ff8f2028d64c50385c3
Matt Parisien <matthewparisien4@gmail.com>
2024-03-20 11:23:59 -0400

    yo

--- plugins/MagneticButton/src/index.js
+++ plugins/MagneticButton/src/index.js
@@ -3,7 +3,9 @@ import "../../../common/styles/reset.css";
 import "./assets/styles/main.css";
 
 const init = () => {
+  console.log('yo!');
   window.addEventListener("load", () => {
+    console.log('hi!');
     const nodes = Array.from(document.querySelectorAll(".sqs-block-button"));
     nodes.forEach((node) => {
       console.log('Hello!');
