2d78a39844566af79d6f79e10656f5c5c18113e5
Matt Parisien <matthewparisien4@gmail.com>
2024-04-02 19:41:25 -0400

    Re-add image trailer

--- src/plugins/MouseFollower/model.ts
+++ src/plugins/MouseFollower/model.ts
@@ -1,8 +1,10 @@
 import gsap from "gsap";
-import * as THREE from "three";
-import { MouseEventsService } from "../_lib/services";
+import {
+  AnimationFrameService,
+  CanvasService,
+  MouseEventsService,
+} from "../_lib/services";
 import { EMouseEvent } from "../_lib/services/MouseEventsService";
-import ShaderService, { TUniforms } from "../_lib/services/ShaderService";
 import { PluginOptions } from "../_lib/ts/types";
 import DomUtils from "../_lib/utils/DomUtils";
 import PluginBase from "../_PluginBase/model";
@@ -10,11 +12,7 @@ import PluginBase from "../_PluginBase/model";
 interface IMouseFollowerOptions {
   color: string;
   radius: number;
-  style: "outline" | "fill";
   speed: number;
-  skewing: number;
-  skewingDelta: number;
-  skewingDeltaMax: number;
 }
 
 interface IMouseFollower {
@@ -27,76 +25,22 @@ interface IMouseFollower {
   lerp(start: number, end: number, amount: number): number;
 }
 
-interface IMouseFollowerUniforms {
-  uMousePosition: THREE.Vector2;
-  uResolution: THREE.Vector2;
-  uTime: number;
-}
-
 class MouseFollower
   extends PluginBase<IMouseFollowerOptions>
   implements IMouseFollower
 {
-  // #region Private members
-
-  private _shaderService: ShaderService | null = null;
-  private _mouseEventsService: MouseEventsService | null = null;
-
-  // #endregion
-
-  // #region Shader programs
-
-  // Vertex shader program
-  private readonly _vsSource: string = `
-      attribute vec4 aVertexPosition;
-      void main(void) {
-          gl_Position = aVertexPosition;
-      }
-  `;
-
-  // Fragment shader program
-  private readonly _fsSource: string = `
-      precision mediump float;
-      uniform vec2 uMousePosition;
-      uniform vec2 uResolution;
-      void main(void) {
-          float radius = 0.05; // Radius of the circle
-          vec2 coords = gl_FragCoord.xy / uResolution.xy;
-          float dist = distance(coords, uMousePosition);
-          if (dist < radius) {
-              gl_FragColor = vec4(1, 0, 0, 1); // Red circle
-          } else {
-              gl_FragColor = vec4(1.0, 0.2, 1.0, 0.3);
-          }
-      }
-  `;
-
-  private _uniforms: TUniforms<IMouseFollowerUniforms> = {
-    uMousePosition: {
-      value: new THREE.Vector2(0, 0),
-    },
-    uResolution: {
-      value: new THREE.Vector2(window.innerWidth, window.innerHeight),
-    },
-    uTime: {
-      value: 0,
-    },
-  };
-
-  // #endregion
+  private _canvasService: CanvasService;
+  private _tickService: AnimationFrameService;
+  private _mouseEventsService: MouseEventsService;
 
   posX = 0;
   posY = 0;
   isDisabled = false;
 
   options: PluginOptions<IMouseFollowerOptions> = {
-    color: "black",
+    color: "red",
     radius: 10,
-    style: "fill",
-    speed: 0.01,
-    skewing: 0.0,
-    skewingDelta: 0.001,
-    skewingDeltaMax: 0.15,
+    speed: 0.1,
   };
 
   constructor(container: any, options: PluginOptions<IMouseFollowerOptions>) {
@@ -105,18 +49,18 @@ class MouseFollower
     this.options = this.validateOptions(options);
     this.options._radius = this.options.radius;
 
-    this._shaderService = new ShaderService(
-      container,
-      this._vsSource,
-      this._fsSource,
-      this._uniforms,
-      this.onRender.bind(this),
-      this.onResize.bind(this)
+    this._canvasService = new CanvasService(
+      this.container as HTMLCanvasElement
     );
-    // this._tickService = new AnimationFrameService(this.onTick.bind(this));
+    this._tickService = new AnimationFrameService(this.onTick.bind(this));
     this._mouseEventsService = new MouseEventsService(window, [
       {
         event: EMouseEvent.Move,
+        handler: this.onMouseMove.bind(this),
+      },
+      {
+        event: EMouseEvent.Enter,
+        handler: this.onMouseEnter.bind(this),
       },
       {
         event: EMouseEvent.Out,
@@ -128,20 +72,36 @@ class MouseFollower
   protected validateOptions(
     options: PluginOptions<IMouseFollowerOptions>
   ): PluginOptions<IMouseFollowerOptions> {
-    if (
-      options.style &&
-      options.style !== "fill" &&
-      options.style !== "outline"
-    ) {
-      options.style = null;
-      console.error(`Option value '${options.style}' is not a valid for style`);
-    }
-
     return this.mergeOptions(options, this.options);
+  }
+
+  resizeCanvas() {
+    this._canvasService.canvas.width = window.innerWidth;
+    this._canvasService.canvas.height = window.innerHeight;
   }
 
   lerp(start, end, t) {
     return start * (1 - t) + end * t;
+  }
+
+  draw() {
+    this._canvasService.context.clearRect(
+      0,
+      0,
+      this._canvasService.context.canvas.width,
+      this._canvasService.context.canvas.height
+    );
+    this._canvasService.context.beginPath();
+    this._canvasService.context.arc(
+      this.posX,
+      this.posY,
+      this.options.radius,
+      0,
+      2 * Math.PI
+    );
+    this._canvasService.context.lineWidth = 5;
+    this._canvasService.context.fillStyle = this.options.color;
+    this._canvasService.context.fill();
   }
 
   scaleIn() {
@@ -163,7 +123,7 @@ class MouseFollower
     }
   }
 
-  onRender(uniforms: TUniforms<IMouseFollowerUniforms>): void {
+  onTick(): void {
     this.posX = this.lerp(
       this.posX,
       this._mouseEventsService.clientX,
@@ -174,15 +134,17 @@ class MouseFollower
       this._mouseEventsService.clientY,
       this.options.speed
     );
-
-    uniforms.uTime.value += 0.01;
-    uniforms.uMousePosition.value.set(this.posX, this.posY);
-    console.log(uniforms.uMousePosition.value);
+    this.draw();
   }
 
-  onResize(uniforms: TUniforms<IMouseFollowerUniforms>): void {
-    uniforms.uResolution.value.set(window.innerWidth, window.innerHeight);
+  onMouseMove(event: MouseEvent): void {
+    if (this.options.radius === 0 && !this.isDisabled) {
+      this.scaleIn();
+      this.isDisabled = true;
+    }
   }
+
+  onMouseEnter(event: MouseEvent): void {}
 
   onMouseOut(event: MouseEvent): void {
     this.scaleOut();
@@ -190,6 +152,7 @@ class MouseFollower
   }
 
   addListeners() {
+    window.addEventListener("resize", this.resizeCanvas.bind(this));
     const links = DomUtils.querySelectorAll(["button", "a"]);
 
     links.forEach((link) => {
@@ -205,8 +168,13 @@ class MouseFollower
   }
 
   init() {
+    this._canvasService.init();
+    this._tickService.init();
     this._mouseEventsService.init();
-    this._shaderService.init();
+
+    this.resizeCanvas();
+    this.addListeners();
+    this.draw();
   }
 }
 
