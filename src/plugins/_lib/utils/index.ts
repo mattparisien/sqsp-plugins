ae00711d3cdafcd9eabfe06e7325f468cba081d0
Matt Parisien <63109316+mattparisien@users.noreply.github.com>
2025-09-03 07:39:40 -0400

    S

--- src/plugins/_lib/utils/index.ts
+++ src/plugins/_lib/utils/index.ts
@@ -100,8 +100,6 @@ export async function initializePlugin(pluginName: string): Promise<void> {
         );
       }
 
-      console.log("is dev", document.querySelector(SQSP_ENV_SELECTOR_MAP.get("DEV")));
-      console.log("document", document);
       isDev = document.querySelector(SQSP_ENV_SELECTOR_MAP.get("DEV"));
       
       if (isDev) {
